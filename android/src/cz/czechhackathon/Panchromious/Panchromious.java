144b81487ed5def5081fd792469c280b0b46aa6f
Bob Koutsky <bohumil.koutsky@gooddata.com>
2014-11-08 02:35:06 +0100

    Create Android app project

---
+++ android/src/cz/czechhackathon/Panchromious/Panchromious.java
@@ -0,0 +1,15 @@
+package cz.czechhackathon.Panchromious;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Panchromious extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
